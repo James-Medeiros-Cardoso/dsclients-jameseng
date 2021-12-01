f4d4d0c7b0b144d3b152852092729115e0f4b30d
James-Medeiros-Cardoso <james-medeiros@hotmail.com>
2021-12-01 14:57:38 -0300

    DTO

--- backend/src/main/java/com/jameseng/dsclients/resources/ClientResource.java
+++ backend/src/main/java/com/jameseng/dsclients/resources/ClientResource.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.jameseng.dsclients.entities.Client;
+import com.jameseng.dsclients.dto.ClientDTO;
 import com.jameseng.dsclients.services.ClientService;
 
 //ClientResource = recurso da classe Client
@@ -21,14 +21,14 @@ public class ClientResource {
 	
 	//primeiro endpoint (primeira rota do recurso):
 	@GetMapping
-	public ResponseEntity<List<Client>> findAll() {
+	public ResponseEntity<List<ClientDTO>> findAll() {
 		
 		/*List<Client> list=new ArrayList<>();
 		//Instant.now()
 		list.add(new Client(1L, "João", "011.355.950-74", 5000.00, Instant.parse("1990-06-25T15:45:00Z"), 2));
 		list.add(new Client(2L, "José", "050.755.624-52", 4000.00, Instant.parse("1985-11-08T09:10:30Z"), 1));*/
 		
-		List<Client> list=service.findAll();
+		List<ClientDTO> list=service.findAll();
 		return ResponseEntity.ok().body(list); //.ok() = deixa reponder resposta 200 = requisição realizada com sucesso
 	}
 
--- backend/src/main/java/com/jameseng/dsclients/services/ClientService.java
+++ backend/src/main/java/com/jameseng/dsclients/services/ClientService.java
@@ -2,11 +2,13 @@
 package com.jameseng.dsclients.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.jameseng.dsclients.dto.ClientDTO;
 import com.jameseng.dsclients.entities.Client;
 import com.jameseng.dsclients.repositories.ClientRepository;
 
@@ -17,8 +19,36 @@ public class ClientService {
 	private ClientRepository repository;
 	
 	@Transactional(readOnly=true) //garante integridade na transação com o banco de dados | readOnly=true = evita looking do banco de dados.
-	public List<Client> findAll() {
-		return repository.findAll();
+	public List<ClientDTO> findAll() {
+		/*PRIMEIRA IMPLEMENTAÇÃO:
+		public List<Client> findAll()
+		return repository.findAll();*/
+		
+		/*SEGUNDA IMPLEMENTAÇÃO:
+		//declara um list do tipo Client que busca todos os clientes
+		List<Client> list=repository.findAll();
+		
+		//declara uma list de ClientDTO
+		List<ClientDTO> listDto=new ArrayList<>();
+		
+		//converte a list de Client em list de ClientDTO:
+		for (Client cli : list) {
+			listDto.add(new ClientDTO(cli));
+		}
+		return listDto;*/
+		
+		/*TERCEIRA IMPLEMENTAÇÃO:
+		//declara um list do tipo Client que busca todos os clientes
+		List<Client> list=repository.findAll();
+		//declara uma list de ClientDTO
+		List<ClientDTO> listDTO=list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+		//.stream() = transforma para stream
+		//.map() = aplica uma função em cada elemento da lista (ou vetor)
+		//.collect(Collectors.toList()) = transforma o stream novamente em uma lista
+		return listDTO;*/
+		
+		List<Client> list=repository.findAll();
+		
+		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
 	}
-	
 }
