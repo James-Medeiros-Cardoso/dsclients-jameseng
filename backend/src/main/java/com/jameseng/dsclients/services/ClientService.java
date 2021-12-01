0215e0f9361354d42ad2d12df66d07394d2e0dd7
James-Medeiros-Cardoso <james-medeiros@hotmail.com>
2021-12-01 14:04:56 -0300

    Database Seeding

--- backend/src/main/java/com/jameseng/dsclients/services/ClientService.java
+++ backend/src/main/java/com/jameseng/dsclients/services/ClientService.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.jameseng.dsclients.entities.Client;
 import com.jameseng.dsclients.repositories.ClientRepository;
@@ -15,9 +16,9 @@ public class ClientService {
 	@Autowired //injeta uma instância gerenciada pelo Spring
 	private ClientRepository repository;
 	
+	@Transactional(readOnly=true) //garante integridade na transação com o banco de dados | readOnly=true = evita looking do banco de dados.
 	public List<Client> findAll() {
 		return repository.findAll();
-
 	}
 	
 }
