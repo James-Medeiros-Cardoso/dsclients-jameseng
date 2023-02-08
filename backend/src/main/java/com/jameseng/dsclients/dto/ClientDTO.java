de705dbbebb8a94d38afa9f8b68080e9c58fcd45
James-Medeiros-Cardoso <james-medeiros@hotmail.com>
2023-02-08 10:17:01 -0300

    Basic beans validation - Aula 03-10 OK

--- backend/src/main/java/com/jameseng/dsclients/dto/ClientDTO.java
+++ backend/src/main/java/com/jameseng/dsclients/dto/ClientDTO.java
@@ -1,89 +1,102 @@
 package com.jameseng.dsclients.dto;
 
+import com.jameseng.dsclients.entities.Client;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Positive;
 import java.io.Serializable;
 import java.time.Instant;
 
-import com.jameseng.dsclients.entities.Client;
-
 public class ClientDTO implements Serializable {
-	private static final long serialVersionUID = 1L;
-	
-	private Long id;
-	private String name;
-	private String cpf;
-	private Double income;
-	private Instant birthDate;
-	private Integer children;
-	
-	public ClientDTO() {
-	}
+    private static final long serialVersionUID = 1L;
 
-	public ClientDTO(Long id, String name, String cpf, Double income, Instant birthDate, Integer children) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.cpf = cpf;
-		this.income = income;
-		this.birthDate = birthDate;
-		this.children = children;
-	}
-	
-	//contrudor que vai povoar a ClientDTO simplismente passando a entidade como argumento
-	public ClientDTO(Client entity) {
-		this.id = entity.getId();
-		this.name = entity.getName();
-		this.cpf = entity.getCpf();
-		this.income = entity.getIncome();
-		this.birthDate = entity.getBirthDate();
-		this.children = entity.getChildren();
-	}
+    private Long id;
 
-	public Long getId() {
-		return id;
-	}
+    @NotEmpty(message = "O campo 'name' é obrigatório.")
+    private String name;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    @CPF(message = "O CPF digitado não é válido.")
+    private String cpf;
 
-	public String getName() {
-		return name;
-	}
+    @Positive(message = "O campo 'income' deve ser positivo.")
+    private Double income;
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    @PastOrPresent(message = "O campo 'birthDate' não pode estar no futuro.")
+    private Instant birthDate;
 
-	public String getCpf() {
-		return cpf;
-	}
+    //@NotEmpty(message = "O campo 'children' não podem estar vazio.")
+    private Integer children;
 
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
+    public ClientDTO() {
+    }
 
-	public Double getIncome() {
-		return income;
-	}
+    public ClientDTO(Long id, String name, String cpf, Double income, Instant birthDate, Integer children) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.income = income;
+        this.birthDate = birthDate;
+        this.children = children;
+    }
 
-	public void setIncome(Double income) {
-		this.income = income;
-	}
+    //contrudor que vai povoar a ClientDTO simplismente passando a entidade como argumento
+    public ClientDTO(Client entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+        this.cpf = entity.getCpf();
+        this.income = entity.getIncome();
+        this.birthDate = entity.getBirthDate();
+        this.children = entity.getChildren();
+    }
 
-	public Instant getBirthDate() {
-		return birthDate;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setBirthDate(Instant birthDate) {
-		this.birthDate = birthDate;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public Integer getChildren() {
-		return children;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setChildren(Integer children) {
-		this.children = children;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public void setIncome(Double income) {
+        this.income = income;
+    }
+
+    public Instant getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Instant birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
+
+    public void setChildren(Integer children) {
+        this.children = children;
+    }
 }
--- backend/src/main/java/com/jameseng/dsclients/resources/ClientResource.java
+++ backend/src/main/java/com/jameseng/dsclients/resources/ClientResource.java
@@ -1,88 +1,61 @@
 package com.jameseng.dsclients.resources;
 
-import java.net.URI;
-
+import com.jameseng.dsclients.dto.ClientDTO;
+import com.jameseng.dsclients.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.jameseng.dsclients.dto.ClientDTO;
-import com.jameseng.dsclients.services.ClientService;
+import javax.validation.Valid;
+import java.net.URI;
 
-//ClientResource = recurso da classe Client
-@RestController    //será um controlador REST que responde por este recurso
-@RequestMapping(value="/clients")	//(value="/clients") = rota REST do recurso
+@RestController
+@RequestMapping(value = "/clients")
 public class ClientResource {
 
-	@Autowired //injeta automaticamente a dependendia gerenciada pelo Spring
-	private ClientService service; 
-	
-	//primeiro endpoint (primeira rota do recurso - mostrar todos os clientes):
-	@GetMapping
-	public ResponseEntity<Page<ClientDTO>> findAll( //parâmetros opcionais:
-			@RequestParam(value = "page", defaultValue = "0") Integer page,
-			@RequestParam(value = "linesPerPage", defaultValue = "6") Integer linesPerPage,
-			@RequestParam(value = "direction", defaultValue = "ASC") String direction,	
-			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
-			) {
-		
-		/*List<Client> list=new ArrayList<>();
-		//Instant.now()
-		list.add(new Client(1L, "João", "011.355.950-74", 5000.00, Instant.parse("1990-06-25T15:45:00Z"), 2));
-		list.add(new Client(2L, "José", "050.755.624-52", 4000.00, Instant.parse("1985-11-08T09:10:30Z"), 1));*/
-		
-		//public ResponseEntity<List<ClientDTO>> findAll()
-		//List<ClientDTO> list=service.findAll();
-		//return ResponseEntity.ok().body(list); //.ok() = deixa reponder resposta 200 = requisição realizada com sucesso
-		
-		PageRequest pageRequest=PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-		
-		Page<ClientDTO> list=service.findAllPaged(pageRequest);
-		
-		return ResponseEntity.ok().body(list); //.ok() = deixa reponder resposta 200 = requisição realizada com sucesso
-	}
-	
-	//segundo endpoint (mostrar cliente por id):
-	@GetMapping(value="/{id}") //acrescentar o id na rota
-	public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
-		//@PathVariabl = para o spring entender que deve vincular o id da rota com o id do finById
-		ClientDTO dto=service.findById(id);
-		return ResponseEntity.ok().body(dto); //.ok() = deixa reponder resposta 200 = requisição realizada com sucesso
-	}
-	
-	//Inserir no banco um novo cliente (INSERT) | @RequestBody = endpoint reconhecer objeto enviado na requisição e vincule o dto*/
-	@PostMapping //padrão REST = inserir um novo recurso = método POST
-	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto){
-		dto=service.insert(dto);
-		URI uri=ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-				.buildAndExpand(dto.getId()).toUri();
-		
-		return ResponseEntity.created(uri).body(dto); //código 201 = recurso criado
-	}
-	
-	//Atualizar um Client por Id (PUT)
-	@PutMapping(value="/{id}") //acrescentar o id na rota
-	public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO dto){
-		dto=service.update(id, dto);
-		return ResponseEntity.ok().body(dto);
-	}
-	
-	//Deletar um Client por Id (DELETE)
-	@DeleteMapping(value="/{id}") //acrescentar o id na rota
-	public ResponseEntity<Void> delete(@PathVariable Long id){
-		service.delete(id);
-		return ResponseEntity.noContent().build(); //vai dar uma resposta 204 = deu certo e o corpo da resposta será vazio
-	}		
+    @Autowired
+    private ClientService service;
+
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAll( //parâmetros opcionais:
+                                                    @RequestParam(value = "page", defaultValue = "0") Integer page,
+                                                    @RequestParam(value = "linesPerPage", defaultValue = "6") Integer linesPerPage,
+                                                    @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+                                                    @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+    ) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+        Page<ClientDTO> list = service.findAllPaged(pageRequest);
+        return ResponseEntity.ok().body(list); //.ok() = deixa reponder resposta 200 = requisição realizada com sucesso
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
+        ClientDTO dto = service.findById(id);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<ClientDTO> insert(@Valid @RequestBody ClientDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(dto); //código 201 = recurso criado
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> update(@PathVariable Long id, @Valid @RequestBody ClientDTO dto) {
+        dto = service.update(id, dto);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build(); //vai dar uma resposta 204 = deu certo e o corpo da resposta será vazio
+    }
 }
