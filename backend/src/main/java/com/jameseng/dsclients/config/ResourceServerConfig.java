a08814934fea10a3dd57f8fad71cafdcdf15bfa0
James-Medeiros-Cardoso <james-medeiros@hotmail.com>
2023-02-20 19:55:52 -0300

    H2 security configuration - Aula 03-27 OK

--- backend/src/main/java/com/jameseng/dsclients/config/ResourceServerConfig.java
+++ backend/src/main/java/com/jameseng/dsclients/config/ResourceServerConfig.java
@@ -2,12 +2,15 @@ package com.jameseng.dsclients.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableResourceServer // implementar o ResourceServer do oauth 2
@@ -16,7 +19,10 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Autowired
     private JwtTokenStore jwtTokenStore;
 
-    private static final String[] PUBLIC = {"/oauth/token"};
+    @Autowired
+    private Environment environment;
+
+    private static final String[] PUBLIC = {"/oauth/token", "/h2-console/**"};
 
     private static final String[] CLIENTS = {"/clients/**"};
 
@@ -29,6 +35,12 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+
+        // liberar H2
+        if (Arrays.asList(environment.getActiveProfiles()).contains("test")) {
+            http.headers().frameOptions().disable();
+        }
+
         http.authorizeRequests()
                 .antMatchers(PUBLIC).permitAll()
 
